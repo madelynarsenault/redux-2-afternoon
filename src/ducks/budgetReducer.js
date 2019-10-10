17625d7522b7e8f3b4d45aa651c82f1376b201c0
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-10-10 14:32:41 -0500

    fixing the live update

--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -40,43 +40,28 @@ export const removePurchase = (id) => {
     }
 }
 
-export default function Reducer(state=initialState, action){
-    switch(action.type){
-        case `${REQUEST_BUDGET_DATA}_PENDING`:
-            return{
-                ...state,
-                loading: true
-            }
-            case `${REQUEST_BUDGET_DATA}_FULFILLED`:
-                return{
-                    ...state,
-                    ...action.payload,
-                    loading:false
-                }
-                case `${ADD_PURCHASE}_PENDING`:
-                    return{
-                        ...state,
-                        loading: true
-                    }
-                    case `${ADD_PURCHASE}_FULFILLED`:
-                        return{
-                            ...state,
-                            ...action.payload,
-                            loading: false
-                        }
-                        case `${REMOVE_PURCHASE}_PENDING`:
-                            return{
-                                ...state,
-                                loading: true
-                            }
-                            case `${REMOVE_PURCHASE}_FULFILLED`:
-                                return{
-                                    ...state,
-                                    ...action.payload,
-                                    loading: false
-                                }
-        default:
-            return state;
+export default function budgetReducer(state = initialState, action) {
+    switch (action.type) {
+      case REQUEST_BUDGET_DATA +
+       '_PENDING':
+        return { ...state, loading: true }
+      case REQUEST_BUDGET_DATA 
+      + '_FULFILLED':
+        return { ...state, ...action.payload, loading: false }
+      case ADD_PURCHASE 
+      + '_PENDING':
+        return { ...state, loading: true }
+      case ADD_PURCHASE
+       + '_FULFILLED':
+        return { ...state, purchases: action.payload, loading: false }
+      case REMOVE_PURCHASE 
+      + '_PENDING':
+        return { ...state, loading: true };
+      case REMOVE_PURCHASE 
+      + '_FULFILLED':
+        return { ...state, loading: false, purchases: action.payload }
+      default:
+        return state;
     }
-}
+  }
 
